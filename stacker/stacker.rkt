1bb80ead3d573d1bb5b85f7b6ef778d2709dee90
Dennis Gosnell <cdep.illabout@gmail.com>
2020-12-22 09:55:21 +0900

    Stacker part 2

--- stacker/stacker.rkt
+++ stacker/stacker.rkt
@@ -16,3 +16,36 @@
   (datum->syntax #f module-datum))
 
 (provide read-syntax)
+
+(define-macro (stacker-module-begin HANDLE-EXPR ...)
+  ;; (printf "in stacker-module-begin, HANDLE-EXPR: ~v\n" #'(HANDLE-EXPR ...))
+  #'(#%module-begin
+     (define (handle [arg #f]) (printf "in handle...\n"))
+     (printf "before running handle expr, ~v...\n" (handle 3))
+     HANDLE-EXPR ...
+     (define hello "hello")
+     (printf "~v ~v\n" (first stack) hello)))
+
+(provide (rename-out [stacker-module-begin #%module-begin]) handle * +)
+
+(define stack empty)
+
+(define (pop-stack!)
+  (define arg (first stack))
+  (set! stack (rest stack))
+  arg)
+
+(define (push-stack! arg)
+  (set! stack (cons arg stack)))
+
+;; (define handle
+;;   (case-lambda
+;;     [() (printf "GOT NOTHING\n")]
+;;     [(x) (printf "GOT SOMETHING: ~v\n" x)]))
+
+(define (handle [arg #f])
+  (cond
+    [(number? arg) (push-stack! arg)]
+    [(or (equal? * arg) (equal? + arg))
+     (define op-result (arg (pop-stack!) (pop-stack!)))
+     (push-stack! op-result)]))
