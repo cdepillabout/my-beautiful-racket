a3aa1b4a516d4cf511904aa8095de973537afd30
Dennis Gosnell <cdep.illabout@gmail.com>
2020-12-25 17:16:39 +0900

    Continue with macro-related tests.

--- stacker/test.rkt
+++ stacker/test.rkt
@@ -54,3 +54,56 @@
     [(_ 25 "hello" mid ... lst)
      (with-syntax ([(one two three) #'lst])
        #'(list mid ... three two one))]))
+
+(define-syntax-rule (swap x y)
+  (let ([tmp (+ x x)])
+    `(y ,tmp)))
+
+(define-syntax-rule (swap_ x y)
+  (let ([tmp x])
+    (set! x y)
+    (set! y tmp)))
+
+
+(let ([tmp 5]
+      [other 6])
+  ;; (let ([tmp tmp])
+  ;;   (set! tmp other)
+  ;;   (set! other tmp))
+  (swap_ tmp other)
+  (list tmp other))
+
+(define-syntax (yoyoval stx)
+  (syntax-case stx ()
+    [yoyoval (identifier? #'yoyoval)
+         #'(+ 1 200)]
+    ))
+
+(define-syntax self-as-string
+  (lambda (stx)
+    (datum->syntax stx
+                   (format "~s" (syntax->datum stx)))))
+
+(define-syntax (self-as-string_ stx)
+  (datum->syntax stx
+                 (format "~s" (syntax->datum stx))))
+
+(define-syntax self-as-string__
+  (syntax-rules ()
+    ;; No way to bind the actual self-as-string identifier when using
+    ;; syntax-rules.
+    [(_ x ...) (format "~s" (syntax->datum #'(x ...)))]
+    )
+  )
+(define-syntax (self-as-string___ stx)
+  (syntax-case stx ()
+    ;; No way to bind the actual self-as-string identifier when using
+    ;; syntax-rules.
+    [(identff x ...) #'(format "~s" (syntax->datum #'(identff x ...)))]
+    )
+  )
+
+(define example-raw-syntax-case
+  (syntax->datum
+   (syntax-case #'(+ 1 2) ()
+     [(op n1 n2) #'(- n1 n2)])))
