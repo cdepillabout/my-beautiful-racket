6cfde5810f0c28a6ae70c29ba97e62edb1175a36
Dennis Gosnell <cdep.illabout@gmail.com>
2021-01-01 14:29:03 +0900

    Try using define-syntax-rule instead of define-syntax and syntax-case.

--- stackerizer-real/stackerizer.rkt
+++ stackerizer-real/stackerizer.rkt
@@ -16,19 +16,26 @@
 
 (provide + *)
 
-(define-syntax (stackerizer-mb stx)
-  (printf "stackerizer-mb, stx: ~v\n" stx)
-  (syntax-case stx (stackerizer-mb)
-    [(stackerizer-mb expr)
-     ;; #'(racket:#%module-begin
-     (printf "stackerizer-mb, in syntax-case, expr: ~v\n" #'expr)
-     #'(racket:#%module-begin (for-each displayln (reverse (flatten expr))))
-     ]
-    [x
-     (printf "stackerizer-mb, in syntax-case, x: ~v\n" #'x)
-     #'(racket:#%module-begin #false)
-     ]
-     ))
+;; Use syntax-case to define a macro.
+;; (define-syntax (stackerizer-mb stx)
+;;   (printf "stackerizer-mb, stx: ~v\n" stx)
+;;   (syntax-case stx (stackerizer-mb)
+;;     [(stackerizer-mb expr)
+;;      ;; #'(racket:#%module-begin
+;;      (printf "stackerizer-mb, in syntax-case, expr: ~v\n" #'expr)
+;;      #'(racket:#%module-begin (for-each displayln (reverse (flatten expr))))
+;;      ]
+;;     ;; This shouldn't get run unless there is some big problem.
+;;     [x
+;;      (printf "stackerizer-mb, in syntax-case, x: ~v\n" #'x)
+;;      #'(racket:#%module-begin #false)
+;;      ]
+;;      ))
+
+;; Use define-syntax-rule to define a macro.
+(define-syntax-rule (stackerizer-mb expr)
+  (racket:#%module-begin (for-each displayln (reverse (flatten expr)))))
+
 (provide
  (rename-out [stackerizer-mb #%module-begin])
  #%app #%datum
@@ -60,20 +67,36 @@
 ;; (define-op *)
 
 ;; This is a version that defines multiple ops at a time.  Good use of `...`.
-(define-syntax (define-ops stx)
-  (syntax-case stx ()
-    [(_ op ...)
-     #'(begin
-         (define-syntax (op stxx)
-           (syntax-case stxx ()
-             [(_ fst) #'fst]
-             [(op fst next (... ...))
-              #'(list 'op fst (op next (... ...)))
-              ]
-             )
-           )
-         ...
-         )
-     ]))
+;; This uses syntax-case.
+;; (define-syntax (define-ops stx)
+;;   (syntax-case stx ()
+;;     [(_ op ...)
+;;      #'(begin
+;;          (define-syntax (op stxx)
+;;            (syntax-case stxx ()
+;;              [(_ fst) #'fst]
+;;              [(op fst next (... ...))
+;;               #'(list 'op fst (op next (... ...)))
+;;               ]
+;;              )
+;;            )
+;;          ...
+;;          )
+;;      ]))
+
+;; Just like above, but use define-syntax-rule.
+(define-syntax-rule (define-ops op ...)
+  (begin
+    (define-syntax (op stxx)
+      (syntax-case stxx ()
+        [(_ fst) #'fst]
+        [(op fst next (... ...))
+         #'(list 'op fst (op next (... ...)))
+         ]
+        )
+      )
+    ...
+    )
+  )
 
 (define-ops + *)
